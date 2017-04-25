3e7feede5f9b35661107e493b4452427906cd710
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2017-04-24 21:29:59 -0400

    Styled it more nicely.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
 import $ from 'jquery';
 import Moment from 'moment';
-import './App.css';
+
+const styles = {
+    label: {
+        fontFamily: 'Helvetica Neue',
+        fontWeight: '200',
+        color: '#808080'
+    },
+    text: {
+        fontFamily: 'Helvetica Neue',
+        fontWeight: '600',
+    }
+}
 
 class App extends Component {
     constructor(props) {
@@ -11,6 +22,7 @@ class App extends Component {
             artist: "",
             title: "",
             imageUrl: "",
+            imageUrlSquare: "",
         };
 
         this.getEverything();
@@ -22,11 +34,16 @@ class App extends Component {
         return Moment(new Date(oneMonthAgo + Math.random() * (today - oneMonthAgo))).format('YYYY-MM-DD');
     }
 
+    getRandomTwoDigit() {
+        let s = "00" + Math.round(Math.random() * 100);
+        return s.substr(2);
+    }
+
     getRandomArtist() {
         $.getJSON(
             "https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&format=json&origin=*",
             (data) => this.setState({
-                artist: Object.values(data.query.pages)[0].title
+                artist: Object.values(data.query.pages)[0].title.replace(/([([])\s*/g, "|").split("|")[0]
             })
         );
     }
@@ -59,15 +76,17 @@ class App extends Component {
     }
 
     getRandomArtwork() {
-        let url = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=9d125f270ce02fcb7b1ebf033e981ea9&date=" + this.getRandomDateInLastMonth() + "&extras=url_l&per_page=10&format=json&nojsoncallback=1";
+        let url = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=9d125f270ce02fcb7b1ebf033e981ea9&date=" + this.getRandomDateInLastMonth() + "&extras=url_l,url_q&per_page=10&format=json&nojsoncallback=1";
         $.getJSON(
             url,
             (data) => {
                 if (data.photos) {
                     let photo = data.photos.photo[Math.round(Math.random() * 10)];
-                    if (photo && "url_l" in photo) {
+                    if (photo && "url_l" in photo && "url_q" in photo) {
                         this.setState({
-                            imageUrl: data.photos.photo[Math.round(Math.random() * 10)].url_l
+                            imageUrl: photo.url_l,
+                            imageUrlSquare: photo.url_q,
+
                         });
                     } else {
                         this.getRandomArtwork();
@@ -98,26 +117,59 @@ class App extends Component {
     render() {
         if (this.isLoading()) {
             return (
-                <div className="App">
-                    <div>
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: '50%',
+                        left: '50%',
+                        webkitTransform: 'translate(-50%, -50%)',
+                        transform: 'translate(-50%, -50%)',
+                    }}
+                >
                         Loading...
-                    </div>
                 </div>
             );
         }
         return (
-            <div className="App">
-                Your artist:
-                <br/>
-                    <span>{this.state.artist}</span>
-                <br/>
-                Your title:
-                <br/>
-                    <span>{this.state.title}</span>
-                <br/>
-                Your artwork:
-                <br/>
-                <img src={this.state.imageUrl} alt=""/>
+            <div
+                style={{
+                    position: 'fixed',
+                    top: '50%',
+                    left: '50%',
+                    WebkitTransform: 'translate(-50%, -50%)',
+                    transform: 'translate(-50%, -50%)',
+                }}
+            >
+                <img
+                    style={{
+                        verticalAlign: 'middle',
+                    }}
+                    src={this.state.imageUrlSquare}
+                    alt=""
+                />
+                <div
+                    style={{
+                        display: 'inline-block',
+                        textAlign: 'left',
+                        verticalAlign: 'middle',
+                        marginLeft: '2em',
+                    }}
+                >
+                    <div>
+                        <span style={styles.label}>Artist: </span>
+                        <span style={styles.text}>{this.state.artist}</span>
+                    </div>
+                    <br/>
+                    <div>
+                        <span style={styles.label}>Album: </span>
+                        <span style={styles.text}>{this.state.title}</span>
+                    </div>
+                    <br/>
+                    <div>
+                        <span style={styles.label}>Length: </span>
+                        <span style={styles.text}>{this.getRandomTwoDigit()}:{this.getRandomTwoDigit()}</span>
+                    </div>
+                </div>
             </div>
         );
     }
